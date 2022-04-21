b96b99649791fecd6fe0a0eb0452c9156c6f31ba
Kevin Clark <kevinwilliamclark@gmail.com>
2022-04-21 15:57:48 -0500

    starting the markers

--- src/MapContainer.jsx
+++ src/MapContainer.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const MapContainer = () => {
-  
+
+  const locations = [
+    {
+      name: "Location 1",
+      location: { 
+        lat: 41.3954,
+        lng: 2.162 
+      },
+    },
+    {
+      name: "Location 2",
+      location: { 
+        lat: 41.3917,
+        lng: 2.1649
+      },
+    },
+    {
+      name: "Location 3",
+      location: { 
+        lat: 41.3773,
+        lng: 2.1585
+      },
+    },
+    {
+      name: "Location 4",
+      location: { 
+        lat: 41.3797,
+        lng: 2.1682
+      },
+    },
+    {
+      name: "Location 5",
+      location: { 
+        lat: 41.4055,
+        lng: 2.1915
+      },
+    }
+  ];
+
   const mapStyles = {        
     height: "100vh",
     width: "100%"};
@@ -16,9 +54,17 @@ const MapContainer = () => {
        googleMapsApiKey='REACT_APP_MAPS_API_KEY'>
         <GoogleMap
           mapContainerStyle={mapStyles}
-          zoom={17}
-          center={defaultCenter}
-        />
+          zoom={13}
+          center={defaultCenter}>
+         {
+            locations.map(item => {
+              console.log(item.name)
+              return (
+              <Marker key={item.name} position={item.location}/>
+              )
+            })
+         }
+     </GoogleMap>
      </LoadScript>
   )
 }
